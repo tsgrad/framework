d6539e24c0896dd4a85df604bd0a33f202e0f5d8
alexwmson <alexrwmson@gmail.com>
2026-08-27 16:28:58 -0400

    more operations

--- src/operators.ts
+++ src/operators.ts
@@ -26,3 +26,31 @@ function eq(a: number, b: number): number{
 function max(a : number, b: number): number{
     return a > b ? a : b;
 }
+
+function is_close(a: number, b: number): number{
+    return Math.abs(a - b) < 1e-2 ? 1 : 0;
+}
+
+function sigmoid(a: number): number{
+    if (a >= 0)
+        return 1.0/(1.0 + Math.exp(-a));  // e^a
+    else
+        return Math.exp(a) / (1.0 + Math.exp(a));
+}
+
+function relu(a: number): number{
+    return Math.max(0, a);
+}
+
+function log(a: number): number{
+    return Math.log(a); // ln(a)
+}
+
+function exp(a: number): number{
+    return Math.exp(a); // e^a
+}
+
+// reciprocal
+function inv(a: number): number{
+    return 1.0 / a;
+}
