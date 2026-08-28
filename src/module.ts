c520b912798e5c0e71fab539b25cb12475b77707
alexwmson <alexrwmson@gmail.com>
2026-08-28 18:51:54 +0200

    Created module class with constructor, train and eval functions

--- src/module.ts
+++ src/module.ts
@@ -31,3 +31,46 @@ class Parameter{
         }
     }
 };
+
+class Module{
+    /*
+        Modules form a tree that store parameters and other
+        submodules. They make up the basis of neural network stacks.
+
+        Attributes
+        ----------
+        _modules : Storage of the child modules
+        _parameters : Storage of the module's parameters
+        training : Whether the module is in training mode or evaluation mode
+    */
+
+    private _modules: Map<String, Module>;
+    private _parameters: Map<String, Parameter>;
+    public training: Boolean; // true means training, false means evaluating
+
+    constructor(){
+        this._modules = new Map<String, Module>;
+        this._parameters = new Map<String, Parameter>;
+        this.training = true;
+    }
+
+    modules(): Module[]{
+        return Array.from(this._modules.values());
+    }
+
+    train(): void{
+        // Sets the training bool of this module and all child modules to true
+        this.training = true;
+        for (const mod  of this.modules()){
+            mod.train();
+        }
+    }
+
+    eval(): void{
+        // Sets the training bool of this module and all child modules to false (meaning eval)
+        this.training = false;
+        for (const mod  of this.modules()){
+            mod.train();
+        }
+    }
+};
