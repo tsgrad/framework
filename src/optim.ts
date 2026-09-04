6d796a92cff74c72c24c810912cb0ae7dc7eea4c
alexwmson <alexrwmson@gmail.com>
2026-09-04 17:32:04 -0400

    Added sgd momentum optimizer

--- src/optim.ts
+++ src/optim.ts
@@ -1,3 +1,4 @@
+import { Queue } from "./datastructures";
 import { Parameter } from "./module";
 import { Scalar } from "./scalar";
 
@@ -6,6 +7,9 @@ export abstract class Optimizer{
     constructor(parameters: Parameter[]){
         this.parameters = parameters;
     }
+
+    abstract zeroGrad(): void;
+    abstract step(): void;
 }
 
 export class SGD extends Optimizer{
@@ -28,6 +32,38 @@ export class SGD extends Optimizer{
         for (const p of this.parameters){
             if (p.value.derivative !== undefined)
                 p.update(new Scalar(p.value.data - this.lr * p.value.derivative));
+        }
+    }
+}
+
+export class SGDMomentum extends Optimizer{
+    lr: number; // learning rate
+    beta: number
+    prev: Map<Parameter, number>;
+    constructor(parameters: Parameter[], lr: number = 1.0, beta: number = 0.9){
+        super(parameters);
+        this.lr = lr;
+        this.beta = beta;
+        this.prev = new Map<Parameter, number>();
+    }
+
+    zeroGrad(): void{
+        for (const p of this.parameters){
+            if ("derivative" in p.value){
+                if (p.value.derivative !== undefined)
+                    p.value.derivative = undefined;
+            }
+        }
+    }
+
+    step(): void{
+        for (const p of this.parameters){
+            if (p.value.derivative !== undefined){
+                let oldVelocity = this.prev.getOrInsert(p, p.value.derivative);
+                let velocity = this.beta * oldVelocity + (1 - this.beta) * p.value.derivative;
+                p.update(new Scalar(p.value.data - this.lr * velocity));
+                this.prev.set(p, velocity);
+            }
         }
     }
 }
