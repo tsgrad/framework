47555be643d65545c2b2e86a3fe28018b73762d2
alexwmson <alexrwmson@gmail.com>
2026-09-11 00:05:41 -0400

    Made broadcastIndex easier to understand

--- src/tensor_data.ts
+++ src/tensor_data.ts
@@ -31,7 +31,7 @@ export function broadcastIndex(bigIndex: Index, bigShape: Shape, smallShape: Sha
         else if (smallShape[i] !== bigShape[i + diff])
             throw "smallShape dimension " + i + " must be 1 or equal to bigShape dimension " + (i + diff);
         else
-            smallIndex.push(bigIndex[i + diff]);
+            smallIndex.push(smallShape[i]);
     }
 }
 
