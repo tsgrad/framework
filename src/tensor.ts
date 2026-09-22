cb53a2e8da41bb1c551a25015ce37a79a80d5fbe
alexwmson <alexrwmson@gmail.com>
2026-09-21 22:46:58 -0400

    Added operators to Tensor class

--- src/tensor.ts
+++ src/tensor.ts
@@ -1,6 +1,6 @@
 import { Context } from "./autodiff";
 import { TensorData, UserShape } from "./tensor_data";
-import { TensorFunction } from "./tensor_functions";
+import { Add, TensorFunction, Neg, Mul, Inv, LT, EQ, IsClose, Sigmoid, ReLU, Log, Exp } from "./tensor_functions";
 import { TensorBackend } from "./tensor_ops";
 
 export class History{
@@ -65,7 +65,73 @@ export class Tensor{
         return this._tensor._storage[0];
     }
 
-    static apply(f: typeof TensorFunction, vals: Tensor[]): Tensor{
+    _ensureTensor(x: Tensor | number){
+        if (x instanceof Tensor)
+            return x;
+        else
+            return new Tensor(new TensorData([x], [1], [1]), undefined, undefined, this.backend)
+    }
+
+    //functions
+
+    add(b: Tensor | number): Tensor{
+        return Tensor.apply(Add, this, this._ensureTensor(b));
+    }
+
+    sub(b: Tensor | number): Tensor{
+        return Tensor.apply(Add, this, Tensor.apply(Neg, this._ensureTensor(b)));
+    }
+
+    mul(b: Tensor | number): Tensor{
+        return Tensor.apply(Mul, this, this._ensureTensor(b));
+    }
+
+    div(b: Tensor | number): Tensor{
+        return Tensor.apply(Mul, this, Tensor.apply(Inv, this._ensureTensor(b)));
+    }
+
+    /* Once we add matrix multiplication
+    matmul(b: Tensor | number): Tensor{
+        return Tensor.apply(MatMul, this, this._ensureTensor(b));
+    }*/
+
+    lt(b: Tensor | number): Tensor{
+        return Tensor.apply(LT, this, this._ensureTensor(b));
+    }
+
+    gt(b: Tensor | number): Tensor{
+        return Tensor.apply(LT, this._ensureTensor(b), this);
+    }
+
+    eq(b: Tensor | number): Tensor{
+        return Tensor.apply(EQ, this, this._ensureTensor(b));
+    }
+
+    isClose(b: Tensor | number): Tensor{
+        return Tensor.apply(IsClose, this, this._ensureTensor(b));
+    }
+
+    neg(): Tensor{
+        return Tensor.apply(Neg, this);
+    }
+
+    sigmoid(): Tensor{
+        return Tensor.apply(Sigmoid, this);
+    }
+
+    relu(): Tensor{
+        return Tensor.apply(ReLU, this);
+    }
+
+    log(): Tensor{
+        return Tensor.apply(Log, this);
+    }
+
+    exp(): Tensor{
+        return Tensor.apply(Exp, this);
+    }
+
+    static apply(f: typeof TensorFunction, ...vals: Tensor[]): Tensor{
         let rawVals: Tensor[] = [];
         let needGrad = false;
 
