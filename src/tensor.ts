c747badafce63b3998df7fd22c648e1a109a075d
alexwmson <alexrwmson@gmail.com>
2026-09-23 19:42:10 -0400

    Added sum and permute member functions to Tensor

--- src/tensor.ts
+++ src/tensor.ts
@@ -1,6 +1,6 @@
 import { Context } from "./autodiff";
 import { TensorData, UserShape, Shape } from "./tensor_data";
-import { Add, TensorFunction, Neg, Mul, Inv, LT, EQ, IsClose, Sigmoid, ReLU, Log, Exp } from "./tensor_functions";
+import { Add, TensorFunction, Neg, Mul, Inv, LT, EQ, IsClose, Sigmoid, ReLU, Log, Exp, Sum, Permute } from "./tensor_functions";
 
 export class History{
     lastFn : typeof TensorFunction | undefined;
@@ -120,6 +120,14 @@ export class Tensor{
 
     exp(): Tensor{
         return Tensor.apply(Exp, this);
+    }
+
+    sum(dim: number): Tensor{
+        return Tensor.apply(Sum(dim), this);
+    }
+
+    permute(...order: number[]): Tensor{
+        return Tensor.apply(Permute(order), this); 
     }
 
     static zeros(shape: Shape): Tensor{
